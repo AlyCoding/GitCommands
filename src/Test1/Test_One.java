91986ccc491a8322f078dddbe6d06308288f122f
mmamyt <91426314+mmamyt@users.noreply.github.com>
2022-05-02 20:47:56 -0500

    adding number

--- src/Test1/Test_One.java
+++ src/Test1/Test_One.java
@@ -4,5 +4,9 @@ public class Test_One {
     public static void main(String[] args) {
         System.out.println("Test One");
 
+        System.out.println("testing");
+        System.out.println("test2");
+        int n = 255;
+
     }
 }
